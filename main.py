a35d36a09c61339b5fcf25cfbaa8cd70a41367a6
Iwada <105448816+Iwada-eborty@users.noreply.github.com>
2022-07-30 05:00:05 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Check if two words are anagrams
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def is_anagram(s1, s2):
+    # [assignment] Add your code here
+    print(sorted(s1), sorted(s2))
+    if (sorted(s1) == sorted(s2)):
+        print(sorted(s1), sorted(s2))
+        return True
+    return False 
+
+print(is_anagram('silent', 'listen'))
